54bd2bd95c4b1182ad56193738bf738f43baafba
victorrrc <121068418+victorrrc@users.noreply.github.com>
2023-02-22 16:07:58 -0800

    Update jk.js

--- jk.js
+++ jk.js
@@ -1,32 +1,31 @@
-const API_KEY = 'O6EPK68gCYoYZh0l0CDPX7R3TDUbOWVI';
-const API_URL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=jennie+kim&limit=25&rating=r`;
+const giphyApiKey = "O6EPK68gCYoYZh0l0CDPX7R3TDUbOWVI";
+const gifSection = document.querySelector(".gif-section");
+const shuffleButton = document.querySelector("#shuffle-button");
 
-
-fetch(API_URL)
-  .then(response => response.json())
-  .then(data => {
-    const gifs = data.data.map(gif => gif.images.fixed_height.url);
-    const shuffledGifs = shuffle(gifs);
-    console.log(shuffledGifs);
-  })
-  .catch(error => console.error(error));
-
-function shuffle(array) {
-  let currentIndex = array.length;
-  let temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
+// Function to get 3 random GIFs and add them to a row
+function getAndAddRandomGifsToRow(rowId) {
+  const giphyUrl = `https://api.giphy.com/v1/gifs/search?api_key=${giphyApiKey}&q=jennie+kim&rating=R&limit=3`;
+  fetch(giphyUrl)
+    .then(response => response.json())
+    .then(data => {
+      const row = document.querySelector(`#${rowId}`);
+      row.innerHTML = "";
+      data.data.forEach(gif => {
+        const gifUrl = gif.images.fixed_width.url;
+        const gifElement = `<img src="${gifUrl}" alt="Jennie Kim GIF" class="col-sm-4">`;
+        row.insertAdjacentHTML("beforeend", gifElement);
+      });
+    });
 }
+
+// Initial loading of 3 random GIFs in each row
+getAndAddRandomGifsToRow("gif-row-1");
+getAndAddRandomGifsToRow("gif-row-2");
+getAndAddRandomGifsToRow("gif-row-3");
+
+// Shuffle button functionality to replace each row with 3 new random GIFs
+shuffleButton.addEventListener("click", () => {
+  getAndAddRandomGifsToRow("gif-row-1");
+  getAndAddRandomGifsToRow("gif-row-2");
+  getAndAddRandomGifsToRow("gif-row-3");
+});
